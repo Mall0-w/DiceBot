e97b73727be6c365149f75c91e3862227eca8525
Mall0w <>
2021-06-08 23:47:30 +0000

    added abstract classes for future trpgs

---
+++ cocClasses/occupations.py
@@ -0,0 +1,12 @@
+class Occupation:
+  @property
+  def name(self):
+    raise NotImplementedError
+  
+  @property
+  def occSkills(self):
+    raise NotImplementedError
+  
+  @property
+  def occPoints(self):
+    raise NotImplementedError
---
+++ dndClasses/classes.py
@@ -0,0 +1,16 @@
+class Class:
+  @property
+  def name(self):
+    raise NotImplementedError
+  
+  @property
+  def hitDice(self):
+    raise NotImplementedError
+  
+  @property
+  def savingThrows(self):
+    raise NotImplementedError
+  
+  @property
+  def abilities(self):
+    raise NotImplementedError
